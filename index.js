4ce0392103bb0aa2f05b9fd2d781ceb8755d9ab4
Isaak Morales <ice.isaak97@gmail.com>
2022-04-07 22:23:27 -0400

    writing the function down

--- index.js
+++ index.js
@@ -3,15 +3,32 @@ const Manager = require("./Manager")
 const Engineer = require("./Engineer")
 const Intern = require("./Intern")
 const inquirer = require("inquirer")
+const InputPrompt = require("inquirer/lib/prompts/input")
 
 function teamBuilder() {
     inquirer
     .prompt([{
-        
+        type: "list",
+        name: "employeeTitle",
+        message: "What's the employee's title",
+        choices: ["Manager", "Engineer", "Intern"]
+        },
+        {when: input => {
+            return input.role === "Manager"
+        },{
+            when: input => {
+                return input.role == "Manager"
+            },
+            type: "input",
+            name: "github",
+            message: "Enter your github username:",
+        },
+
+        }
+
+
+    
+
 
     }])
-
-    
-
-    
 }
