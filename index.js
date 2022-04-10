80d0c131a653db193939e0659040dc70346da759
Isaak Morales <ice.isaak97@gmail.com>
2022-04-10 14:46:57 -0400

    Got the layout on html

--- index.js
+++ index.js
@@ -2,30 +2,60 @@ const Employee = require("./Employee")
 const Manager = require("./Manager")
 const Engineer = require("./Engineer")
 const Intern = require("./Intern")
-const inquirer = require("inquirer")
 const InputPrompt = require("inquirer/lib/prompts/input")
+const fs = require ("fs")
+const inquirer = require("inquirer")
+const markdown = require("./generateMarkdown.js")
+
 
 function teamBuilder() {
     inquirer
     .prompt([{
         type: "list",
-        name: "employeeTitle",
+        name: " input.employeeTitle",
         message: "What's the employee's title",
         choices: ["Manager", "Engineer", "Intern"]
         },
         {
             when: input => {
-                return input.role == "Manager"
+                return input.role === "Manager"
             },
             type: "input",
-            name: "office",
+            name: "officeNumber",
             message: "Office Number:",
         },
-
-        
-
-
+        {
+            when: input => {
+                return input.role === "Engineer"
+            },
+            type: "input",
+            name: "github",
+            message: "Github username:",
+        },
+        {
+            when: input => {
+                return input.role === "Intern"
+            },
+            type: "input",
+            name: "school",
+            message: "School Name:",
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your id?",
+        },
     
+    
+    
+    
+    ]).then((answer) => {
+        console.log(answer.input.employeeTitle, answer.officeNumber, answer.github, answer.school, answer.name, answer.id);
 
-
-])}
+        fs.writeFile("index.html", markdown({ ...answer }), (err) => err ? console.log(err) : console.log("Success"))
+    })}
