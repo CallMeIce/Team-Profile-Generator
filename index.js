529c80669ecc34c4e16cf1846cb69a20a1598ae2
Isaak Morales <ice.isaak97@gmail.com>
2022-04-10 16:28:05 -0400

    Working on index.js

--- index.js
+++ index.js
@@ -1,44 +1,21 @@
-const Employee = require("./Employee")
-const Manager = require("./Manager")
-const Engineer = require("./Engineer")
-const Intern = require("./Intern")
-const InputPrompt = require("inquirer/lib/prompts/input")
-const fs = require ("fs")
-const inquirer = require("inquirer")
-const markdown = require("./generateMarkdown.js")
+const Employee = require("./Employee");
+const Manager = require("./Manager");
+const Engineer = require("./Engineer");
+const Intern = require("./Intern");
+const InputPrompt = require("inquirer/lib/prompts/input");
+const fs = require ("fs");
+const inquirer = require("inquirer");
+const markdown = require("./generateMarkdown.js");
+const teamCreation = [];
 
 
 function teamBuilder() {
     inquirer
     .prompt([{
         type: "list",
-        name: " input.employeeTitle",
+        name: "employeeTitle",
         message: "What's the employee's title",
         choices: ["Manager", "Engineer", "Intern"]
-        },
-        {
-            when: input => {
-                return input.role === "Manager"
-            },
-            type: "input",
-            name: "officeNumber",
-            message: "Office Number:",
-        },
-        {
-            when: input => {
-                return input.role === "Engineer"
-            },
-            type: "input",
-            name: "github",
-            message: "Github username:",
-        },
-        {
-            when: input => {
-                return input.role === "Intern"
-            },
-            type: "input",
-            name: "school",
-            message: "School Name:",
         },
         {
             type: "input",
@@ -50,12 +27,50 @@ function teamBuilder() {
             name: "id",
             message: "What is your id?",
         },
-    
-    
-    
-    
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Office Number:",
+            when: answer => {
+                if (answer.employeeTitle === "Manager") {
+                    return true;
+                }
+            },
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Github username:",
+            when: answer => {
+                if (answer.employeeTitle === "Engineer") {
+                    return true;
+                }
+            },
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "School Name:",
+            when: answer => {
+                if (answer.employeeTitle === "Intern") {
+                    return true;
+                }
+            },
+        },
+        // {
+        //     type: "list",
+        //     name: "addAnother",
+        //     message: "Would you like to add another employee?",
+        //     choices: ["yes", "no"],
+        //     if (answer.addAnother = "yes") {
+        //         teamBuilder();
+        //     }else {
+                
+        //     }
+        // }
+        
     ]).then((answer) => {
-        console.log(answer.input.employeeTitle, answer.officeNumber, answer.github, answer.school, answer.name, answer.id);
+        console.log(answer.employeeTitle, answer.officeNumber, answer.github, answer.school, answer.name, answer.id);
 
         fs.writeFile("index.html", markdown({ ...answer }), (err) => err ? console.log(err) : console.log("Success"))
     })}
