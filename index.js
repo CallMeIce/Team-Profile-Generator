c00aa7c9d940e8f1b645272c3a4bbba79f65154b
Isaak Morales <ice.isaak97@gmail.com>
2022-04-11 19:13:31 -0400

    finished writing prompts just gotta do the done/push to html

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Employee = require("./Employee");
 const Manager = require("./Manager");
 const Engineer = require("./Engineer");
 const Intern = require("./Intern");
-const InputPrompt = require("inquirer/lib/prompts/input");
 const fs = require("fs");
 const inquirer = require("inquirer");
 const markdown = require("./generateMarkdown.js");
@@ -11,36 +10,36 @@ const teamCreation = [];
 function managerCard() {
     inquirer
         .prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is your manager's name?",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is your manager's id?",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is your manager's email?",
-        },
-        {
-            type: "input",
-            name: "officeNumber",
-            message: "Manager's Office Number:",
-        }])
+            {
+                type: "input",
+                name: "name",
+                message: "What is your manager's name?",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your manager's id?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your manager's email?",
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "Manager's Office Number:",
+            }])
         .then((answer) => {
-            const manager = new Manager (
+            const manager = new Manager(
                 answer.name, answer.id, answer.email, answer.officeNumber
             )
             console.log(manager);
             teamCreation.push(manager);
             teamBuilder();
-    });
-    } 
-    managerCard();
+        });
+}
+managerCard();
 
 function teamBuilder() {
     inquirer
@@ -55,32 +54,66 @@ function teamBuilder() {
                 //* Call engineer function
                 function engineerCard() {
                     inquirer
-        .prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is your manager's name?",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is your manager's id?",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is your manager's email?",
-        },
-        {
-            type: "input",
-            name: "officeNumber",
-            message: "Manager's Office Number:",
+                        .prompt([
+                            {
+                                type: "input",
+                                name: "name",
+                                message: "What is your engineer's name?",
+                            },
+                            {
+                                type: "input",
+                                name: "id",
+                                message: "What is your manager's id?",
+                            },
+                            {
+                                type: "input",
+                                name: "email",
+                                message: "What is your engineer's email?",
+                            },
+                            {
+                                type: "input",
+                                name: "officeNumber",
+                                message: "Engineer's GitHub Profile:",
+                            }
+                        ]) 
                 }
+                console.log(Engineer);
+                teamCreation.push(Engineer);
+                engineerCard();
             } else if (answer.employeeTitle === "Intern") {
                 //*Call intern function
+                function internCard() {
+                    inquirer
+                        .prompt([
+                            {
+                                type: "input",
+                                name: "name",
+                                message: "What is your intern's name?",
+                            },
+                            {
+                                type: "input",
+                                name: "id",
+                                message: "What is your intern's id?",
+                            },
+                            {
+                                type: "input",
+                                name: "email",
+                                message: "What is your intern's email?",
+                            },
+                            {
+                                type: "input",
+                                name: "school",
+                                message: "What is your intern's school name?:",
+                            }
+                        ])
+                }
+                console.log(Intern);
+                teamCreation.push(Intern);
+                internCard();
             } else {
                 //*Call the done and write everything to html file
             }
+
         })
 }
 
