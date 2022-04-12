acb2ad25246f6bc7974007e9c8b81268c01e7096
Isaak Morales <ice.isaak97@gmail.com>
2022-04-11 22:46:44 -0400

    working on tests

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,25 +1,6 @@
 const Engineer = require("../Engineer")
 
-test("user input id", () => {
-    const id = 10
-    const emp = new Engineer(id)
-    expect(emp.id).toBe(id)
-})
-
-test("user input name", () => {
-    const name = "Frank"
-    const empName =  new Engineer(name)
-    expect(empName.name).toBe(name)
-})
-
-test("user input email", () => {
-    const email = "email@test.com"
-    const empEmail =  new Engineer(email)
-    expect(empEmail.email).toBe(email)
-})
-
-test("user input Github profile", () => {
-    const github = "Github"
-    const empGithub =  new Engineer(github)
-    expect(empGithub.github).toBe(github)
-})
+test("Engineer test id, name, email, and github", () => {
+    const engineer = new Engineer("Bob", "id25", "test@test.com", "profile")
+    expect(engineer.getGithub().toBe("profile"))
+} )
